94ce50ef6fff2e81c2cdfe956b33ce701671b225
rosiv <rosiu@outlook.com>
2015-11-02 23:06:57 +0100

    gem install bundler

---
+++ binaries/Ruby22-x64/bin/bundler.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"d:\ruby\binaries\Ruby22-x64\bin\ruby.exe" "d:/ruby/binaries/Ruby22-x64/bin/bundler" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"d:\ruby\binaries\Ruby22-x64\bin\ruby.exe" "%~dpn0" %*
